8b7fa5168392c5818fb1d90e0af745e42079211e
B-Litty <bradlitman94@gmail.com>
2022-03-16 16:53:40 -0700

    added download cv on home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,6 +5,7 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import Avatar from '../../assets/home.jpg'
+import Resume from '../../assets/BradLitman-Resume.docx'
 
 
 const styles = makeStyles({
@@ -14,7 +15,8 @@ const styles = makeStyles({
         fontSize: "3em"
     },
     avatar: {
-        height: "40vh"
+        height: "40vh",
+        marginBottom: "50px",
     },
     homeContainer: {
         display: "flex",
@@ -25,7 +27,8 @@ const styles = makeStyles({
     bioContainer: {
         display: "flex",
         flexDirection: "column",
-        alignSelf: "center"
+        alignSelf: "center",
+        marginBottom: "100px"
     },
     bioText: {
         width: "75%"
@@ -37,6 +40,12 @@ const styles = makeStyles({
         color: "white",
         margin: "5px",
         marginTop: "20px"
+    },
+    cv: {
+        border: "none",
+        background: "none",
+        color: "white",
+        marginLeft: "35%"
     }
 })
 
@@ -47,16 +56,16 @@ export default function Navbar() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 2, delay: 0.5 }}
-            exit={{opacity: 0, transition: {duration: 1}}}
+            exit={{ opacity: 0, transition: { duration: 1 } }}
         >
             <div>
                 <h2 className={classes.title}>Welcome to my Portfolio!</h2>
                 <div className={classes.homeContainer}>
-                    <motion.img alt="Wife and I" src={Avatar} className={classes.avatar} animate={{borderRadius: ["49% 51% 80% 20% / 30% 30% 70% 70%", "65% 35% 59% 41% / 30% 30% 70% 70%", "49% 51% 80% 20% / 30% 30% 70% 70%"]}} transition={{repeat: Infinity, duration: 5}}/>
+                    <motion.img alt="Wife and I" src={Avatar} className={classes.avatar} animate={{ borderRadius: ["49% 51% 80% 20% / 30% 30% 70% 70%", "65% 35% 59% 41% / 30% 30% 70% 70%", "49% 51% 80% 20% / 30% 30% 70% 70%"] }} transition={{ repeat: Infinity, duration: 5 }} />
                     <div className={classes.bioContainer}>
-                        <h3 style={{fontSize: "35px"}}>Bradley Litman</h3>
+                        <h3 style={{ fontSize: "35px" }}>Bradley Litman</h3>
                         <div>
-                        <p className={classes.bioText}><i>Full Stack Web Developer in the Phoenix Area that loves to learn, exercise, play games and read</i></p>
+                            <p className={classes.bioText}><i>Full Stack Web Developer in the Phoenix Area that loves to learn, exercise, play games and read</i></p>
                             <motion.button className={classes.icons} whileHover={{
                                 scale: 1.5
                             }}>
@@ -80,6 +89,15 @@ export default function Navbar() {
                                     <GitHubIcon />
                                 </a>
                             </motion.button>
+
+                            <a href={Resume} download >
+                                <motion.button className={classes.cv} whileHover={{
+                                    scale: 1.2,
+                                    textShadow: "0px 0px 8px rgb(248,24,148)"
+                                }}>
+                                    Download CV
+                                </motion.button>
+                            </a>
                         </div>
                     </div>
                 </div>
